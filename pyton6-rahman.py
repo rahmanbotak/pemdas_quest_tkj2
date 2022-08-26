c31035fa5301105e571034788415d2eba88acbf2
rahmanbotak <111481890+rahmanbotak@users.noreply.github.com>
2022-08-26 09:27:01 +0700

    Add files via upload

---
+++ pyton6-rahman.py
@@ -0,0 +1,39 @@
+print("1. persegi/n")
+
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah: ", luas_p)
+
+print("2. persegi panjang")
+
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar"))
+luas_pp = panjang * lebar
+print("luas persegi panjang adalah: ", luas_pp)
+
+print("3. segitiga")
+
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tingg: "))
+luas_s = alas * tinggi / 2
+print("luas segitiga adalah:", luas_s)
+
+print("4. jajar genjang")
+
+alas = int(input("masukkan nilai alas: "))
+tinggi = int(input("'masukkan niai tinggi: "))
+luas_jg = alas * tinggi
+print("luas jajar genjang adalah:", luas_jg)
+
+print(". segitiga")
+
+alas = int(input("masukan nilai panjang: "))
+tinggi = int(input("masukan nilai lebar: "))
+luas_s = alas = tinggi / 2
+print("luas segitiga adalah", luas_s)
+
+print(". lingkaran")
+
+r = int(input("masukan nilai jari jari: "))
+luas_1 =r * r * 3.14
+print("luas lingkaran adalah: ")
